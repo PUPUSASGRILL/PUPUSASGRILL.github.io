da555a0d62901d54b6c2ef290b4c2dd326de28e4
Diego Andino <115840719+ItsDMAF@users.noreply.github.com>
2024-11-30 23:06:04 +0100

    Add files via upload
    First Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+const openPopup = document.getElementById('openPopup');
+        const closePopup = document.getElementById('closePopup');
+        const popup = document.getElementById('popup');
+
+        openPopup.addEventListener('click', function (e) {
+            e.preventDefault();
+            popup.style.display = 'flex';
+            document.body.classList.add('no-scroll');
+        });
+
+        closePopup.addEventListener('click', function () {
+            popup.style.display = 'none';
+            document.body.classList.remove('no-scroll');
+        });
+
+        const openImagePopup = document.getElementById('openImagePopup');
+        const closeImagePopup = document.getElementById('closeImagePopup');
+        const imagePopup = document.getElementById('imagePopup');
+
+        openImagePopup.addEventListener('click', function (e) {
+            e.preventDefault();
+            imagePopup.style.display = 'flex';
+            document.body.classList.add('no-scroll'); 
+        });
+
+        closeImagePopup.addEventListener('click', function () {
+            imagePopup.style.display = 'none'; 
+            document.body.classList.remove('no-scroll'); 
+        });
